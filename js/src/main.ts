4b92daf5ab744099c7b835020b66a60a0ac5e1a3
navigaid <navigaid@gmail.com>
2019-11-17 01:02:21 +0800

    add experimental hterm support

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,11 +1,24 @@
+import {lib} from "libapps";
 import { Xterm } from "./xterm";
+import { Hterm } from "./hterm";
 import { Terminal, WebTTY, protocols } from "./webtty";
 import { ConnectionFactory } from "./websocket";
 
 const elem = document.getElementById("terminal")
 
 if (elem !== null) {
-    var term: Terminal = new Xterm(elem);
+    console.log(window.location.hash)
+    var term: Terminal;
+    if (window.location.hash == '#hterm') {
+        console.log("before lib.init");
+        var onInit = ()=>{console.log("lib.init")};
+        (<any>lib).init(onInit);
+        console.log("after lib.init");
+        term = new Hterm(elem);
+    } else {
+        term = new Xterm(elem);
+    }
+    console.log("after new Xterm/Hterm");
     const httpsEnabled = window.location.protocol == "https:";
     const url = (httpsEnabled ? 'wss://' : 'ws://') + window.location.host + window.location.pathname + 'ws';
     const factory = new ConnectionFactory(url, protocols);
