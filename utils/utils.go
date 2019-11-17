86c76d9854ca8843b54d1dc7108d28ff4b65a30f
navigaid <navigaid@gmail.com>
2019-11-17 22:09:47 +0800

    simplify utils.WsWrapper

--- utils/utils.go
+++ utils/utils.go
@@ -3,19 +3,9 @@ package utils
 import (
 	"io"
 	"io/ioutil"
-	"log"
-	"os"
 
 	"github.com/gorilla/websocket"
 )
-
-func Expand(path string) string {
-	if path[0:2] == "~/" {
-		return os.Getenv("HOME") + path[1:]
-	} else {
-		return path
-	}
-}
 
 // WsWrapper makes a io.ReadWriter from websocket.Conn, implementing the wetty.Master interface
 // it is fed to wetty.New to create a WeTTY, bridging the websocket.Conn and local command
@@ -32,30 +22,25 @@ func (wsw *WsWrapper) Write(p []byte) (n int, err error) {
 	return writer.Write(p)
 }
 
-func (wsw *WsWrapper) Read(p []byte) (int, error) {
-	for {
-		msgType, reader, err := wsw.Conn.NextReader()
-		if err != nil {
-			return 0, err
-		}
-
-		if msgType != websocket.BinaryMessage {
-			continue
-		}
-
-		// n, err := reader.Read(p)
-		all, err := ioutil.ReadAll(reader)
-		// if len(rem) != 0 {
-		//	log.Println("REMAINING BUF NOT READ:", err2)
-		// }
-		copy(p, all)
-		n := len(all)
-		if len(all) > len(p) {
-			n = len(p)
-			log.Println("NOT ALL BYTES ARE COPIED")
-		}
-		return n, err
+func (wsw *WsWrapper) Read(buf []byte) (int, error) {
+	_, reader, err := wsw.Conn.NextReader()
+	if err != nil {
+		return 0, err
 	}
+
+	msg, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return 0, err
+	}
+
+	copy(buf, msg)
+
+	n := len(msg)
+	if n > len(buf) {
+		n = len(buf)
+	}
+
+	return n, nil
 }
 
 // ReadWriter stores pointers to a Reader and a Writer.
