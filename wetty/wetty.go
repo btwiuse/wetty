f6a9dc6fd58089f5fe8072cfafc0ee66a91e6d51
navigaid <navigaid@gmail.com>
2019-11-23 18:48:50 +0800

    fix upgrade:websocket: request origin not allowed by Upgrader.CheckOrigin

--- wetty/wetty.go
+++ wetty/wetty.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"net/http"
 
 	"github.com/gorilla/websocket"
 	"github.com/kr/pty"
@@ -46,6 +47,7 @@ var (
 	Upgrader = &websocket.Upgrader{
 		Subprotocols:      Protocols,
 		EnableCompression: true,
+		CheckOrigin:       func(r *http.Request) bool { return true },
 	}
 )
 
