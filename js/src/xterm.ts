257fe7bba7dd1baf2693e5d41acefb28d50387ce
navigaid <navigaid@gmail.com>
2020-01-07 15:22:48 +0800

    add experimental attach

--- js/src/xterm.ts
+++ js/src/xterm.ts
@@ -1,6 +1,7 @@
 import { Terminal } from "xterm";
 import { WebglAddon } from 'xterm-addon-webgl';
 import { FitAddon } from 'xterm-addon-fit';
+import { AttachAddon } from 'xterm-addon-attach';
 import { UTF8Decoder } from 'libdot';
 
 // https://stackoverflow.com/questions/30106476/using-javascripts-atob-to-decode-base64-doesnt-properly-decode-utf-8-strings
@@ -12,7 +13,8 @@ export class Xterm {
     term: Terminal;
     fit: FitAddon;
     webgl: WebglAddon;
-    resizeListener: () => void;
+    resizeListener: (event: Event) => void;
+    resizeListenerWithContext: (event: Event) => void;
     decoder: UTF8Decoder;
 
     message: HTMLElement;
@@ -26,19 +28,20 @@ export class Xterm {
             allowTransparency: true
         });
         this.fit = new FitAddon();
-        this.webgl = new WebglAddon();
+        this.webgl = new WebglAddon(false);
         this.decoder = new UTF8Decoder();
 
         this.message = elem.ownerDocument.createElement("div");
         this.message.className = "xterm-overlay";
         this.messageTimeout = 2000;
 
-        this.resizeListener = () => {
+        this.resizeListener = (event: Event) => {
             console.log("resize:", this.info());
             this.fit.fit();
             this.term.scrollToBottom();
             this.showMessage(String(this.term.cols) + "x" + String(this.term.rows), this.messageTimeout);
         };
+        this.resizeListenerWithContext = this.resizeListener.bind(this);
 
         this.term.open(elem);
 
@@ -46,12 +49,16 @@ export class Xterm {
         this.term.loadAddon(this.fit);
 
         // onopen
-        this.resizeListener();
-        window.addEventListener("resize", () => { this.resizeListener(); });
+        // this.resizeListener();
+        window.addEventListener("resize", this.resizeListenerWithContext);
 
         this.fit.fit();
         this.term.onSelectionChange(() => {
             console.log("onSelectionChange:", this.term.getSelection());
+        });
+        this.term.onResize((data) => {
+            console.log("onresize", data.cols, data.rows);
+            // callback(data.cols, data.rows);
         });
     };
 
@@ -61,6 +68,12 @@ export class Xterm {
 
     output(data: string) {
         this.term.write(this.decoder.decode(data));
+    };
+
+    attach(ws: WebSocket) {
+        const attachAddon = new AttachAddon(ws, {bidirectional: true});
+        console.log("loadAddon(attachAddon)");
+        this.term.loadAddon(attachAddon);
     };
 
     showMessage(message: string, timeout: number) {
@@ -108,7 +121,11 @@ export class Xterm {
     }
 
     close(): void {
-        window.removeEventListener("resize", this.resizeListener);
-        this.term.dispose();
+        console.log("close called on Terminal::Xterm");
+        window.removeEventListener("resize", this.resizeListenerWithContext);
+        // once window listener is unregistered, term won't receive any resize event
+        // this.onResize((cols: number, rows: number) => {console.log(cols + rows);});
+        // dispose not implemented on webgl backend
+        // this.term.dispose();
     }
 }
