5c6364624a3259ff41393b942339057aaa16354e
navigaid <navigaid@gmail.com>
2020-01-07 15:03:30 +0800

    drop hterm

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,6 +1,4 @@
-import { lib } from "libapps";
 import { Xterm } from "./xterm";
-import { Hterm } from "./hterm";
 import { Terminal, WeTTY, protocols } from "./wetty";
 import { ConnectionFactory } from "./websocket";
 // import { ConnectionFactory } from "./grpc";
@@ -11,21 +9,10 @@ if (elem !== null) {
     console.log(window.location.hash)
 
     // term (frontend)
-    var term: Terminal;
-    if (window.location.hash == '#hterm') {
-        console.log("before lib.init");
-        var onInit = ()=>{console.log("lib.init")};
-        (<any>lib).init(onInit);
-        console.log("after lib.init");
-        term = new Hterm(elem);
-    } else {
-        term = new Xterm(elem);
-    }
-    console.log("after new Xterm/Hterm");
+    var term : Terminal = new Xterm(elem);
 
     // factory (websocket backend)
-    const httpsEnabled = window.location.protocol == "https:";
-    const url = (httpsEnabled ? 'wss://' : 'ws://') + window.location.host + window.location.pathname + 'ws';
+    const url = (window.location.protocol == "https:" ? 'wss://' : 'ws://') + window.location.host + window.location.pathname + 'ws';
     const factory = new ConnectionFactory(url, protocols);
 
     // factory (grpc backend)
@@ -35,8 +22,8 @@ if (elem !== null) {
     */
 
     // wetty (hub)
-    const wt = new WeTTY(term, factory);
-    const closer = wt.open();
+    const wetty = new WeTTY(term, factory);
+    const closer = wetty.open();
 
     window.addEventListener("unload", () => {
         closer();
