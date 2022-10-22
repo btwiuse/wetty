d70aa8278335c35b949cbbebe1690aeddbf25edb
navigaid <navigaid@gmail.com>
2022-10-23 01:58:55 +0800

    fix: xterm height: 100%

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, {useEffect, useState} from "react";
 import Console from "./Console";
+import "./App.css";
 
 const Style = {
-  left: '0',
-  right: '0',
-  buttom: '0',
-  top: '0',
-  width: '100%',
   height: '100%',
-  position: 'absolute',
 };
 
 export function App() {
