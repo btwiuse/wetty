532f787bf3668c2a659e30d65d861c24e54460d4
navigaid <navigaid@gmail.com>
2022-10-23 01:21:54 +0800

    add reset/scrollbar css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "./App";
 import "./index.css";
+import "./reset.css";
+import "./scrollbar.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
